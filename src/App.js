0872d56b280a4d40b431172c34eb9421f776edc1
Brian Barquesi <barquesi.tech@gmail.com>
2022-06-13 11:52:13 -0300

    defining props into Card component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,47 @@ import { Card } from './components/card';
 import { getElements } from './services/client-services';
 
 import Nav from './components/nav';
-const element = getElements();
-console.log(element);
+
+const sectorLineup = document.querySelector('.lineup-list')
+
+
+async function llamarClientes(){
+  try {
+    const data = await getElements()
+    if (data != null) {
+      // ({Title, Map, Site, Abilitý,img_position , img_lineup, video_preview})
+      data.forEach( elemento => {
+        const tarjetaLineup = <Card 
+        Title={elemento.Title}
+        Map={elemento.Map}
+        Site={elemento.Site}
+        Ability={elemento.Ability}
+        img_position={elemento.img_position}
+        img_lineup={elemento.img_lineup}
+        video_preview={elemento.video_preview}
+        />;
+        console.log(tarjetaLineup)
+        
+      })
+    }else{
+      throw new Error();
+    }
+  } catch (error) {
+    
+  }
+}
+
+llamarClientes();
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <Nav />
           <main>
-            
+                 <div className='lineup-list'>
+
+                 </div>
           </main>
       </header>
     </div>
--- src/services/client-services.js
+++ src/services/client-services.js
@@ -1,4 +1,4 @@
-const url = "https://briobarbtech.github.io/viper/db.json/";
+const url = "https://briobarbtech.github.io/viper/db.json";
 
 export function getElements () {
     return fetch(url).then( respuesta => {
